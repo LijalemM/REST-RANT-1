fc47b5c78bfc0c24eb593e8bde2e4fa9b24da999
Your Name <you@example.com>
2023-01-26 20:47:55 -0800

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -27,6 +27,20 @@ app.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+app.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id] })
+  }
+})
+
+
 
 
 
