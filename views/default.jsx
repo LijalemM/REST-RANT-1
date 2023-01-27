8cc5f982e47d1c2817d28880d18c74811266e643
Your Name <you@example.com>
2023-01-26 20:25:32 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -10,13 +10,28 @@ const React = require('react')
                       https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css
                       " rel="stylesheet"></link>
                 </head>
-                <body>
+            <body>
                 <script src="
                      https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js
                      "></script>
+                     <nav>
+                        <ul>
+                            <li> 
+                                <a href='/'>Home</a>
+                            </li>
+                            <li> 
+                                <a href='/places'>Places</a>
+                            </li>
+                            <li> 
+                                <a href='/places/new'>Add Places</a>
+                            </li>
+                             </ul>
+                     </nav>
+
                     {html.children}
-                </body>
-            </html>
+                    
+            </body>
+        </html>
         )
       }
       
