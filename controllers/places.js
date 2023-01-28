158af7071fef4e40ecbd431368bef18ae5e4b547
Your Name <you@example.com>
2023-01-28 12:44:34 -0800

    Delete Functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -36,9 +36,24 @@ app.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', {place: places[id] })
+    res.render('places/show', {place: places[id], id})
   }
 })
+
+app.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.slice(id,1)
+    res.redirect('/places')
+  }
+})
+
 
 
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,20 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{data.place.name}</h1>
+            <h2>{data.place.pic}</h2>
+            <h3>{data.place.city}</h3>
+            <h4>{data.place.state}</h4>
+            <h5>{data.place.cuisines}</h5>
+            <a href={`/places/${data.id}/edit`} className='btn btn-warning'> Edit
+            </a>
+            <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+              <button type='submit' className='btn btn-danger'> Delete </button>
+            </form>
           </main>
         </Def>
     )
